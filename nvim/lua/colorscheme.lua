55333cc48ea8cc62a7f0312360cebb5ec0a56572
Barret Ren <Barret.Ren@vecima.com>
2023-02-16 10:37:36 +0800

    add settings for diffview

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,8 +1,8 @@
 -- 样式
-vim.o.background = "dark"
+vim.o.background = "light"
 vim.o.termguicolors = true
-vim.opt.termguicolors = true
-local colorscheme = "doom-one"
+vim.g.solarized_italics = 0
+local colorscheme = "solarized8_flat"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
     vim.notify("colorscheme " .. colorscheme .. " not Found")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -168,6 +168,31 @@ require'nvim-tree'.setup {
 
 require("diffview").setup{
     use_icons = false,
+    view = {
+        -- Configure the layout and behavior of different types of views.
+        -- Available layouts: 
+        --  'diff1_plain'
+        --    |'diff2_horizontal'
+        --    |'diff2_vertical'
+        --    |'diff3_horizontal'
+        --    |'diff3_vertical'
+        --    |'diff3_mixed'
+        --    |'diff4_mixed'
+        -- For more info, see ':h diffview-config-view.x.layout'.
+        default = {
+            -- Config for changed files, and staged files in diff views.
+            layout = "diff2_horizontal",
+        },
+        merge_tool = {
+            -- Config for conflicted files in diff views during a merge or rebase.
+            layout = "diff4_mixed",
+            disable_diagnostics = true,   -- Temporarily disable diagnostics for conflict buffers while in the view.
+        },
+        file_history = {
+            -- Config for changed files in file history views.
+            layout = "diff2_horizontal",
+        },
+    },
 }
 
 local cmp = require'cmp'
