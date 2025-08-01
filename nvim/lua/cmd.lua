6be8634e58b87715e6bb5e1ce78c0203faa3a445
renxiuhu <renxiuhu@outlook.com>
2025-08-01 09:31:17 +0800

    nvim close No Name buffer after file is loaded

--- nvim/lua/cmd.lua
+++ nvim/lua/cmd.lua
@@ -2,6 +2,34 @@
 vim.api.nvim_create_autocmd("BufReadPost", {
     pattern = { "*" },
     callback = function()
+            -- Get a list of all buffers
+            local buffers = vim.api.nvim_list_bufs()
+            -- Iterate over each buffer
+            for _, bufnr in ipairs(buffers) do
+                -- Check if the buffer is empty and doesn't have a name
+                if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_name(bufnr) == '' and
+                    vim.api.nvim_buf_get_option(bufnr, 'buftype') == '' then
+
+                    -- Get all lines in the buffer
+                    local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+
+                    -- Initialize a variable to store the total number of characters
+                    local total_characters = 0
+
+                    -- Iterate over each line and calculate the number of characters
+                    for _, line in ipairs(lines) do
+                        total_characters = total_characters + #line
+                    end
+
+                    -- Close the buffer if it's empty:
+                    if total_characters == 0 then
+                        vim.api.nvim_buf_delete(bufnr, {
+                            force = true
+                        })
+                    end
+                end
+            end
+
         if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
             vim.fn.setpos(".", vim.fn.getpos("'\""))
             -- how do I center the buffer in a sane way??
